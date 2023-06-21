83f26d7c262111e8650df955934ed50d40ccdd98
inamfalahuddin <inamfalahuddin06@gmail.com>
2023-06-21 15:35:24 +0700

    add teknis

---
+++ src/data/layanan/dataProfilInstruktur.js
@@ -0,0 +1,79 @@
+import ImageAgus from "../images/layanan/teknis/Agus-Wahyudi.png";
+import ImageAnton from "../images/layanan/teknis/Anton-Effendi.png";
+import ImageArief from "../images/layanan/teknis/Arief-Wibowo.png";
+import ImageEko from "../images/layanan/teknis/Eko-Anggoro.png";
+import ImageFarid from "../images/layanan/teknis/Farid-Mahendra.png";
+import ImageJarot from "../images/layanan/teknis/Jarot-Sutono.png";
+import ImageMulyanto from "../images/layanan/teknis/Mulyanto.png";
+import ImagePurnomo from "../images/layanan/teknis/Purnomo-Respati.png";
+import ImagePutra from "../images/layanan/teknis/Putra-Adi-Widrajat.png";
+
+const dataProfilInstruktur = [
+  {
+    kategori: "Gambar Teknik",
+    nama: "Eko Anggoro",
+    peran: "Instruktur Gambar Teknik",
+    pengalaman: "3 tahun",
+    image: ImageEko,
+  },
+  {
+    kategori: "CNC",
+    nama: "Farid Mahendra",
+    peran: "Programmer Mesin CNC",
+    pengalaman: "3 tahun",
+    image: ImageFarid,
+  },
+  {
+    kategori: "CNC",
+    nama: "Jarot Sutono",
+    peran: "Programer Mesin CNC",
+    pengalaman: "3 tahun",
+    image: ImageJarot,
+  },
+  {
+    kategori: "Welding",
+    nama: "Arief Wibowo",
+    peran: "Instruktur Welding",
+    pengalaman: "13 tahun",
+    image: ImageArief,
+  },
+  {
+    kategori: "Milling",
+    nama: "Anton Efendi",
+    peran: "Instruktur Milling",
+    pengalaman: "11 tahun",
+    image: ImageAnton,
+  },
+  {
+    kategori: "Milling",
+    nama: "Purnomo Respati",
+    peran: "Instruktur Milling",
+    pengalaman: "10 tahun",
+    image: ImagePurnomo,
+  },
+  {
+    kategori: "Bubut",
+    nama: "Putra Adi Widrajat",
+    peran: "Instruktur Bubut",
+    pengalaman: "14 tahun",
+    image: ImagePutra,
+  },
+  {
+    kategori: "Benchwork",
+    nama: "Agus Wahyudi",
+    peran: "Instruktur Benchwork",
+    pengalaman: "11 tahun",
+    image: ImageAgus,
+  },
+  {
+    kategori: "Grinding",
+    nama: "Mulyanto",
+    peran: "Instruktur Grinding",
+    pengalaman: "12 tahun",
+    image: ImageMulyanto,
+  },
+];
+
+const dataProfilInstrukutrJSON = JSON.stringify(dataProfilInstruktur);
+
+export default dataProfilInstrukutrJSON;
