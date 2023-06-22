b80596dccbef12b99dbbeebde454efd417e5ca63
unknown <hasan.2001@students.amikom.ac.id>
2023-06-22 14:33:46 +0700

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,137 +4,156 @@ import LogoSTP from "@/images/Logo-Technopark-Remake-Solo-1.webp";
 import Link from "next/link";
 import KawasanSTP from "@/images/site-room.png";
 import Barcode from "@/images/barcode-150x150.png";
+import { BsBuildingFill } from "react-icons/bs";
+import { FiMapPin } from "react-icons/fi";
+import { AiFillPhone, AiOutlineMail } from "react-icons/ai";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram, faLinkedin, faTiktok, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+  faTiktok,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import logoWhite from "../images/Logo-Technopark-Remake-Solo-1.webp";
 
 function Footer() {
   return (
-    <footer className="bg-primary-gradient-100">
-      <div className="container py-20">
-        <div className="grid grid-cols-1 md:grid-cols-3 text-white">
-          <div>
-            <Image src={LogoSTP} alt="logo STP" />
-            <p className="my-5">
-              Menuju kawasan Solo Technopark yang inovatif dan berdaya saing internasional.
+    <footer>
+      <div className="bg-primary-100 bg-gradient-patern py-24">
+        <div className="container mx-auto flex flex-wrap">
+          <div className="w-full lg:w-1/3 px-4">
+            <Image src={logoWhite} alt="Logo" width={200} height={85} />
+            <p className="text-white text-base my-5 mb-5">
+            Menuju kawasan Solo Technopark yang inovatif dan berdaya saing internasional.
             </p>
-            <div className="grid grid-cols-3">
-              <div>
-                <ul className="list-inline">
-                  <li className="list-inline-item">
-                    <Link target="_blank" href="https://www.instagram.com/solotechnopark_official/">
-                      <FontAwesomeIcon
-                        icon={faInstagram}
-                        className="footer-icon smaller-icon"
-                        style={{ width: '30px', height: '30px' }}
-                      />
-                    </Link>
-                  </li>
-                  <li className="list-inline-item">
-                    <Link target="_blank" href="https://twitter.com/solotechnopark_">
-                      <FontAwesomeIcon
-                        icon={faTwitter}
-                        className="footer-icon smaller-icon"
-                        style={{ width: '30px', height: '30px' }}
-                      />
-                    </Link>
-                  </li>
-                </ul>
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+              <div className="flex mb-2 items-center">
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  className="mr-2 text-white"
+                  width="20px"
+                />
+                <Link
+                  href="https://www.facebook.com/profile.php?id=100080288423574"
+                  className="text-white hover:text-gray-300"
+                >
+                  Facebook
+                </Link>
               </div>
-              <div>
-                <ul className="list-inline">
-                  <li className="list-inline-item">
-                    <Link target="_blank" href="https://www.facebook.com/solotechnopark.id">
-                      <FontAwesomeIcon
-                        icon={faFacebook}
-                        className="footer-icon smaller-icon"
-                        style={{ width: '30px', height: '30px' }}
-                      />
-                    </Link>
-                  </li>
-                  <li className="list-inline-item">
-                    <Link target="_blank" href="https://www.tiktok.com/@solotechnopark?lang=id-ID">
-                      <FontAwesomeIcon
-                        icon={faTiktok}
-                        className="footer-icon smaller-icon"
-                        style={{ width: '30px', height: '30px' }}
-                      />
-                    </Link>
-                  </li>
-                </ul>
+              <div className="flex mb-2 items-center">
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  className="mr-2 text-white"
+                  width="20px"
+                />
+                <Link
+                  href="https://twitter.com/Soloincubator"
+                  className="text-white hover:text-gray-300"
+                >
+                  Twitter
+                </Link>
               </div>
-              <div>
-                <ul className="list-inline">
-                  <li className="list-inline-item">
-                    <Link target="_blank" href="https://www.linkedin.com/company/solo-technopark/">
-                      <FontAwesomeIcon
-                        icon={faLinkedin}
-                        className="footer-icon smaller-icon"
-                        style={{ width: '30px', height: '30px' }}
-                      />
-                    </Link>
-                  </li>
-                  <li className="list-inline-item">
-                    <Link target="_blank" href="https://www.youtube.com/channel/UCqtTWpV2tExmQ1pQQLILd2Q">
-                      <FontAwesomeIcon
-                        icon={faYoutube}
-                        className="footer-icon smaller-icon"
-                        style={{ width: '30px', height: '30px' }}
-                      />
-                    </Link>
-                  </li>
-                </ul>
+              <div className="flex mb-2 items-center">
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  className="mr-2 text-white"
+                  width="20px"
+                />
+                <Link
+                  href="https://www.instagram.com/solotechnoincubator/"
+                  className="text-white hover:text-gray-300"
+                >
+                  Instagram{" "}
+                </Link>
+              </div>
+              <div className="flex mb-2 items-center">
+                <FontAwesomeIcon
+                  icon={faTiktok}
+                  className="mr-2 text-white"
+                  width="20px"
+                />
+                <Link
+                  href="https://www.tiktok.com/@solotechnoincubator?lang=id-ID"
+                  className="text-white hover:text-gray-300"
+                >
+                  Tiktok{" "}
+                </Link>
+              </div>
+              <div className="flex mb-2 items-center">
+                <FontAwesomeIcon
+                  icon={faYoutube}
+                  className="mr-2 text-white"
+                  width="20px"
+                />
+                <Link
+                  href="https://www.youtube.com/channel/UCqtTWpV2tExmQ1pQQLILd2Q"
+                  className="text-white hover:text-gray-300"
+                >
+                  Youtube{" "}
+                </Link>
+              </div>
+              <div className="flex mb-2 items-center">
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  className="mr-2 text-white"
+                  width="20px"
+                />
+                <Link
+                  href="https://www.linkedin.com/company/solo-technopark/"
+                  className="text-white hover:text-gray-300"
+                >
+                  LinkedIn{" "}
+                </Link>
               </div>
             </div>
           </div>
 
-          <div>
-            <h4 className="head-4">Kontak</h4>
-            <div className="mb-5">
-              <h5 className="font-bold">Alamat:</h5>
-              <Link
-                target="_blank"
-                href="https://www.google.co.id/maps/place/Solo+Techno+Park/@-7.5555128,110.8536264,18z/data=!4m6!3m5!1s0x2e7a16e2b5ffa643:0xa0bf36ec85b94dfb!8m2!3d-7.5560692!4d110.8538665!16s%2Fg%2F1pp2tkfp9?hl=en"
-              >
+          <div className="w-full lg:w-1/3 px-4 mt-10 lg:mt-0">
+            <h2 className="font-bold text-white mb-5">Contact Us</h2>
+            <div className="flex items-center mb-2">
+              <BsBuildingFill className="mr-3 text-white " />
+              <p className="text-white">Gedung Solo Trade Center</p>
+            </div>
+            <div className="flex mb-2 items-center">
+              <FiMapPin className="mr-3 text-white" />
+              <p className="text-white">
                 Jl. Ki Hajar Dewantara No. 19 Jebres, Kec. Jebres, Kota Surakarta
-              </Link>
+              </p>
             </div>
-            <div className="mb-5">
-              <h5 className="font-bold">Telepon:</h5>
-              <Link target="_blank" href="">
-                (+62)271666628
-              </Link>
+            <div className="flex mb-2 items-center">
+              <AiFillPhone className="mr-3 text-white" />
+              <p className="text-white">0857-4151-5573</p>
             </div>
-            <div className="mb-5">
-              <h5 className="font-bold">Email:</h5>
-              <Link
-                target="_blank"
-                href="info@solotechnopark.id/?subject=Tanya%20Produk&body=Saya%20Tertarik%20dengan%20Layanan%20Anda"
-              >
-                info@solotechnopark.id
-              </Link>
-            </div>
-            <div>
-              <h5 className="font-bold">Marketing:</h5>
-              <Link
-                target="_blank"
-                href="https://api.whatsapp.com/send/?phone=%2B6285743110623&text&type=phone_number&app_absent=0"
-              >
-                085743110623
-              </Link>
+            <div className="flex mb-2 items-center">
+              <AiOutlineMail className="mr-3 text-white" />
+              <p className="text-white">info@solotechnoincubator.id</p>
             </div>
           </div>
-          <div className="mt-5">
-            <Image src={KawasanSTP} alt="kawasan STP" />
-            <Link href="https://s.id/contactsolotechnopark?sid-qr=1" target="_blank">
-              <Image className="w-24 mr-0 ml-auto mt-5" src={Barcode} alt="kawasan STP" />
-            </Link>
+          <div className="w-full lg:w-1/3 px-4 mt-10 lg:mt-0">
+            <h2 className="font-bold text-white mb-5">Find Us</h2>
+            <div className="mt-2 overflow-hidden rounded-lg">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.173350482129!2d110.8516778147767!3d-7.556069194550727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a16e2b5ffa643%3A0xa0bf36ec85b94dfb!2sSolo%20Techno%20Park!5e0!3m2!1sid!2sid!4v1680755929146!5m2!1sid!2sid"
+                width="100%"
+                height="350"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+            </div>
           </div>
         </div>
       </div>
-      <div className="bg-white text-slate-600 text-center pt-5">
-        <p>Copyright © 2023 Solo Technopark, All rights reserved.</p>
-        <p className="text-sm mt-2">Designed and Developed by MSIB Batch 4.</p>
-      </div>
+      <p className="bg-white text-black text-center py-5">
+      Copyright © 2023 Solo Technopark, All rights reserved.
+        <br /> Designed and Developed by{" "}
+        <Link href={"/"} className="text-secondary-200">
+        MSIB Batch 4.
+        </Link>
+      </p>
     </footer>
   );
 }
